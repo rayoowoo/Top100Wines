cb05110a62a60c9acd10a4e9feed1b39dde64367
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-03 00:48:30 -0400

    Edit file structure

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
 
 module.exports = router;
