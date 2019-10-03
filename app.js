6df959669b1fd7cf025852167c71221c7570fc3e
Ruiyu <ruiyuwu1998@gmail.com>
2019-10-03 01:15:12 -0400

    Slim down express backend

--- app.js
+++ app.js
@@ -1,44 +1,23 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
 var cors = require('cors');
-
-var indexRouter = require('./routes/index');
-var winesRouter = require('./routes/wines');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(cors());
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.use('/', indexRouter);
-// app.use('/users', usersRouter);
-app.use('/api/wines', winesRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/api/wines', (req, res) => {
+  res.send(wines);
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.get('/api/wines/:id', (req, res) => {
+  const wine = wines.find(el => el.id === req.params.id);
+  res.send(wine);
+})
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+  console.log("hi")
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
 });
 
 
